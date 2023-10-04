c37195a57b13e8ad8c69c17802d57aea02f3a7f5
Robin Pahlman <robin.pahlman@gmail.com>
2023-10-04 17:43:25 -0400

    add cards to projects component

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -5,5 +5,69 @@
 <template>
   <main>
     <div class=""><h1>Projects</h1></div>
+    <!-- CARD 1 -->
+    <div
+      class="flex flex-col rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:max-h-xl md:flex-row hover:bg-darkred">
+      <img
+        class="h-28 p-4 w-100 rounded-t-lg object-cover md:h-50 md:!rounded-none md:!rounded-l-lg"
+        :src="'/public/img/rapid-fire-screenshot.png'"
+        alt="" />
+      <div class="flex flex-col justify-start p-6">
+        <h5
+          class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Rapid Fire
+        </h5>
+        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          Rapid Fire is a dynamic quiz template for ecommerce and more. The quiz includes user email input and shows a dynamic results view that corresponds with the user's answers. 
+        </p>
+        <div class="flex py-4">
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            Vue.js
+          </button>
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            Typescript
+          </button>
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            CI/CD
+          </button>
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            Cloudfront
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- END CARD 1 -->
+    <!-- CARD 2 -->
+    <div
+      class="flex flex-col rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:max-h-xl md:flex-row hover:bg-darkred">
+      <img
+        class="h-28 p-4 w-100 rounded-t-lg object-cover md:h-50 md:!rounded-none md:!rounded-l-lg"
+        :src="'/public/img/rapid-fire-screenshot.png'"
+        alt="" />
+      <div class="flex flex-col justify-start p-6">
+        <h5
+          class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Rapid Fire
+        </h5>
+        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          Rapid Fire is a dynamic quiz template for ecommerce and more. The quiz includes user email input and shows a dynamic results view that corresponds with the user's answers. 
+        </p>
+        <div class="flex py-4">
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            Vue.js
+          </button>
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            Typescript
+          </button>
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            CI/CD
+          </button>
+          <button class="bg-green text-black text-xs mr-4 py-2 px-4 rounded-full">
+            Cloudfront
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- END CARD 2 -->
   </main>
 </template>
