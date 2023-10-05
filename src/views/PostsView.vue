af2fda01107a078c4266aced8bbeeefa49701d79
Robin Pahlman <robin.pahlman@gmail.com>
2023-10-05 10:02:17 -0400

    plug in django api

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -4,9 +4,9 @@
             <h1>Blog</h1>
             <ul>
                 <li v-for="post in posts" :key="post.id">
-                    <h2>{{ post.title }}</h2>
-                    <p>by {{ post.owner }}</p>
-                    <p>{{ post.body }}</p>
+                    <h2 class="text-2xl py-2" v-html="post.title"></h2>
+                    <p class="py-2" v-html="`by ${post.owner}`"></p>
+                    <p class="py-2" v-html="post.blurb"></p>
                     
                     <!-- <button @click="deletePost(deletePost)">Delete</button> -->
                 </li>
@@ -20,7 +20,7 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios'
 let posts = ref()
 onMounted(async() => {
-    const res = await axios.get(`http://localhost:8000/api/v1/posts/`);
+    const res = await axios.get(`http://env-rp-music-dev.eba-sxduvmc3.us-east-1.elasticbeanstalk.com/api/v1/posts`);
     posts.value = res.data
 });
 </script>
